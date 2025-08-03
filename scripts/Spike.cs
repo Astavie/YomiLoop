1ba476bb153510149489bc874e544398fad1e0c4
Ssoulner <delinda.lortie@gmail.com>
2025-08-03 17:28:51 +0200

    Level stuff

--- scripts/Spike.cs
+++ scripts/Spike.cs
@@ -4,6 +4,6 @@ public partial class Spike : Area2D
 {
     private void OnBodyEntered(Node2D body)
     {
-        if (body is Robo robo) robo.IsDead = true;
+        if (body is Robo robo) Robo.Loop.OnFrame(robo, 0);
     }
 }
